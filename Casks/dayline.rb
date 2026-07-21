675e4b0144cacb24c9672ad9226c70b31662a512
Robin | Liquidium <75593191+robin-liquidium@users.noreply.github.com>
2026-07-21 21:12:24 +0200

    Update Dayline cask to 0.1.9 (#2)

--- Casks/dayline.rb
+++ Casks/dayline.rb
@@ -1,6 +1,6 @@
 cask "dayline" do
-  version "0.1.8"
-  sha256 "df2f834cb69c90f04e052fb138771522c1c868f11d7fe381282169e9f8b06b30"
+  version "0.1.9"
+  sha256 "26a8c7eaead8a9edd05468a41a290a64704b8cec3834eb2eea58ef9e0305fe01"
 
   url "https://github.com/robin-liquidium/dayline/releases/download/v#{version}/Dayline-#{version}.dmg",
       verified: "github.com/robin-liquidium/dayline/"
