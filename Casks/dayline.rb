7a6878ca4c173659d05d86c1db913d13c7ea7757
Robin | Liquidium <github.com.thermal146@passmail.net>
2026-07-24 17:46:03 +0200

    dayline 0.1.11

--- Casks/dayline.rb
+++ Casks/dayline.rb
@@ -1,6 +1,6 @@
 cask "dayline" do
-  version "0.1.10"
-  sha256 "6e5c4427a3b9e84e7ea9165fee7366c98efee08b58396e382abca9c119744bcc"
+  version "0.1.11"
+  sha256 "a4206675afdbb0dc063de56ea0d58f0a2b4a2e44136e99361f295d0e630d5540"
 
   url "https://github.com/robin-liquidium/dayline/releases/download/v#{version}/Dayline-#{version}.dmg",
       verified: "github.com/robin-liquidium/dayline/"
