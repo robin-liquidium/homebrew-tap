3e6f03bfe26e7fceaf0169971d574e3fced84c92
Robin | Liquidium <75593191+robin-liquidium@users.noreply.github.com>
2026-07-30 00:58:25 +0200

    dayline 0.1.13 (#6)

--- Casks/dayline.rb
+++ Casks/dayline.rb
@@ -1,6 +1,6 @@
 cask "dayline" do
-  version "0.1.12"
-  sha256 "4ffe6b044bc74f915be4ee83997d7f064cbd893dfe58aeb9af9401538f3dcbd6"
+  version "0.1.13"
+  sha256 "71ca3a95a55d655f4f9429f985d965bf54433577bb766871207742e4552289ed"
 
   url "https://github.com/robin-liquidium/dayline/releases/download/v#{version}/Dayline-#{version}.dmg",
       verified: "github.com/robin-liquidium/dayline/"
