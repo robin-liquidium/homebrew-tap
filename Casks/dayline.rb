41d026982d28f396925bb7d361b2d41f24ff13e7
Robin | Liquidium <github.com.thermal146@passmail.net>
2026-09-07 03:56:49 +0700

    Remove deprecated cask URL verification option

--- Casks/dayline.rb
+++ Casks/dayline.rb
@@ -2,8 +2,7 @@ cask "dayline" do
   version "0.2.8"
   sha256 "5259d6bc89fc685d40889d426aebb449aff02ee51cc4aa9d7807b2cb4c473557"
 
-  url "https://github.com/robin-liquidium/dayline/releases/download/v#{version}/Dayline-#{version}.dmg",
-      verified: "github.com/robin-liquidium/dayline/"
+  url "https://github.com/robin-liquidium/dayline/releases/download/v#{version}/Dayline-#{version}.dmg"
   name "Dayline"
   desc "Menu bar calendar and Linear companion"
   homepage "https://dayline.robin.build/"
